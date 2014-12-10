01580219b2d0ad713280332955ccf39272adffaf
Vincent Jousse <vincent@jousse.org>
2014-12-10 13:53:52 +0100

    id => _id to map the Mongo id

--- app/user/User.scala
+++ app/user/User.scala
@@ -2,7 +2,7 @@ package readflow.user
 
 import reactivemongo.bson.BSONObjectID
 
-case class User(id: BSONObjectID, accessToken: String)
+case class User(_id: BSONObjectID, accessToken: String)
 
 object User {
   import reactivemongo.bson.Macros
